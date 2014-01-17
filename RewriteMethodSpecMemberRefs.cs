ffe01e62f52de1af9fb1acb255d139ab3c69af34
Ignas Ziberkas <ignas@unity3d.com>
2014-01-17 17:55:33 +0200

    Use MethodDefs and FieldDefs to reference members in the same module (prevents MDIL from crashing).

--- RewriteMethodSpecMemberRefs.cs
+++ RewriteMethodSpecMemberRefs.cs
@@ -1,9 +1,28 @@
-﻿using Mono.Cecil;
+﻿using System;
+using Mono.Cecil;
 
 namespace Unity.ReferenceRewriter
 {
 	class RewriteMethodSpecMemberRefs : RewriteStep, IMethodDefinitionVisitor
 	{
+		private static string GetAssemblyName(IMetadataScope scope)
+		{
+			switch (scope.MetadataScopeType)
+			{
+				case MetadataScopeType.AssemblyNameReference:
+					return ((AssemblyNameReference)scope).Name;
+				case MetadataScopeType.ModuleDefinition:
+					return ((ModuleDefinition)scope).Assembly.Name.Name;
+				default:
+					throw new NotSupportedException(string.Format("Metadata scope type {0} is not supported.", scope.MetadataScopeType));
+			}
+		}
+
+		private static bool IsSameScope(IMetadataScope left, IMetadataScope right)
+		{
+			return string.Equals(GetAssemblyName(left), GetAssemblyName(right), StringComparison.Ordinal);
+		}
+
 		protected override void Run()
 		{
 			MethodDefinitionDispatcher.DispatchOn(Context.TargetModule, this);
@@ -16,24 +35,57 @@ namespace Unity.ReferenceRewriter
 
 			foreach (var instruction in method.Body.Instructions)
 			{
-				var genericInstanceMethod = instruction.Operand as GenericInstanceMethod;
-
-				if ((genericInstanceMethod != null) && (genericInstanceMethod.DeclaringType.MetadataType != MetadataType.GenericInstance))
+				var typeReference = instruction.Operand as TypeReference;
+				if (typeReference != null)
 				{
-					var elementMethod = genericInstanceMethod.ElementMethod;
+					if (typeReference.IsDefinition)
+						continue;
+					if (typeReference.IsArray || typeReference.IsGenericParameter || typeReference.IsGenericInstance)
+						continue;
+					if (IsSameScope(typeReference.Scope, method.DeclaringType.Scope))
+						instruction.Operand = typeReference.Resolve();
+					continue;
+				}
 
-					if (elementMethod.MetadataToken.TokenType == TokenType.MemberRef)
+				var memberReference = instruction.Operand as MemberReference;
+				if (memberReference != null)
+				{
+					if (memberReference.IsDefinition || memberReference.DeclaringType.IsGenericInstance)
+						continue;
+
+					var methodReference = memberReference as MethodReference;
+					if (methodReference != null)
 					{
-						var elementMethodDefinition = elementMethod.Resolve();
-
-						if (elementMethodDefinition.DeclaringType.Module == method.DeclaringType.Module)
+						var genericInstanceMethod = methodReference as GenericInstanceMethod;
+						if (genericInstanceMethod != null)
 						{
-							var genericInstanceMethodFixed = new GenericInstanceMethod(elementMethodDefinition);
-							foreach (var argument in genericInstanceMethod.GenericArguments)
-								genericInstanceMethodFixed.GenericArguments.Add(argument);
-							instruction.Operand = genericInstanceMethodFixed;
+							var elementMethod = genericInstanceMethod.ElementMethod.Resolve();
+							if (IsSameScope(elementMethod.DeclaringType.Scope, method.DeclaringType.Scope))
+							{
+								var genericInstanceMethodFixed = new GenericInstanceMethod(elementMethod);
+								foreach (var argument in genericInstanceMethod.GenericArguments)
+									genericInstanceMethodFixed.GenericArguments.Add(argument);
+								instruction.Operand = genericInstanceMethodFixed;
+							}
 						}
+						else
+						{
+							if (IsSameScope(methodReference.DeclaringType.Scope, method.DeclaringType.Scope))
+								instruction.Operand = methodReference.Resolve();
+						}
+						continue;
 					}
+					
+
+					var fieldReference = memberReference as FieldReference;
+					if (fieldReference != null)
+					{
+						if (IsSameScope(fieldReference.DeclaringType.Scope, method.DeclaringType.Scope))
+							instruction.Operand = fieldReference.Resolve();
+						continue;
+					}
+
+					continue;
 				}
 			}
 		}
